7877fac8824a7e4e43d2b8c0e6523a9bb7fb477e
Hariom Nayma <74749873+hariom-nayma@users.noreply.github.com>
2022-03-03 19:54:10 +0800

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -44,7 +44,7 @@ if (Config.WORKTYPE == 'private') {
             );
         
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ *HarioM Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );    
         } else {
 
